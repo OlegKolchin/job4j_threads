e7a59ee28d205e034a989da1f0268cf73834cfbd
OlegKolchin <commshepard@gmail.com>
2021-10-08 13:40:58 +0500

    1. Неблокирующий кеш [#4741]

--- src/main/java/ru/job4j/cache/Cache.java
+++ src/main/java/ru/job4j/cache/Cache.java
@@ -2,7 +2,6 @@ package ru.job4j.cache;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.function.BiFunction;
 
 public class Cache {
     private final Map<Integer, Base> memory = new ConcurrentHashMap<>();
@@ -12,16 +11,15 @@ public class Cache {
     }
 
     public boolean update(Base model) {
-        return memory.computeIfPresent(model.getId(), function(model)) != null;
-    }
-
-    private BiFunction<Integer, Base, Base> function(Base model) {
         Base stored = memory.get(model.getId());
-        if (stored.getVersion() != model.getVersion()) {
-            throw new OptimisticException("Versions are not equal");
-        }
-        BiFunction<Integer, Base, Base> rsl = (a, b) -> b = new Base(model.getId(), model.getVersion() + 1);
-        return rsl;
+        return memory.computeIfPresent(stored.getId(), (key, value) -> {
+            if (stored.getVersion() != model.getVersion()) {
+                throw new OptimisticException("Versions are not equal");
+            } else {
+                value = new Base(model.getId(), model.getVersion() + 1);
+                return value;
+            }
+        }) != null;
     }
 
     public void delete(Base model) {
